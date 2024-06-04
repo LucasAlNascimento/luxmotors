6e13e7a9bb4f38cc8c3229d45091e5954a371ca1
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-06-03 21:09:49 -0300

    Arrumando caminho relativo ícone Brazil

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="flex justify-between items-center w-full h-20 px-5 bg-gray-200 shadow-md md:px-11 mt-auto">
             <p>© 2023 Luxmotors Car Rentals Inc.</p>
             <div className="flex items-center justify-center gap-2">
-                <img className="w-8 rounded-md" src="/src/assets/icons/Brazil.png" alt="Ícone Brasil"/>
+                <img className="w-8 rounded-md" src="/assets/icons/Brazil.png" alt="Ícone Brasil"/>
                 <p className="text-sm font-fabrikatMedium">Brazil</p>
             </div>
         </footer>
